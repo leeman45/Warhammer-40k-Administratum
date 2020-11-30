37cb6f64d96d2972c862de6bb74c4e3bcb9667b2
Matthew Shibata <shibatam@sheridancollege.ca>
2020-11-29 21:03:15 -0500

    Add project files.

---
+++ Warhammer 40k Administratum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Warhammer_40k_Administratum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
